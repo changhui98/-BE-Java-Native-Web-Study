ab63c58c6ebce4fa9da9adccd544945c8c4ac7a5
changhui98 <net_0102@naver.com>
2025-05-13 16:53:22 +0900

    first commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.core;
+
+import hello.core.member.*;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new memberA = " + memberA.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}
