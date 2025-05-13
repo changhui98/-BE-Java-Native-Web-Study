b05611e21991cd9a78fd921289179ba7b7765f4e
changhui98 <net_0102@naver.com>
2025-05-13 17:18:58 +0900

    [BE] 주문 도메인 생성 , 서비스 계층 로직 구현 , 할인 (discount) 고정 정책 로직 구현

---
+++ src/main/java/hello/core/discount/fixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class fixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; // 1,000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            // Enum 타입은 == 쓰는 게 맞음 .
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
