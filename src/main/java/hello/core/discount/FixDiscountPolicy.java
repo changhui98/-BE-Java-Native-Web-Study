ac8dbf7b8ee2a3efec48a9deb9641897a8c70fff
changhui98 <net_0102@naver.com>
2025-05-13 22:46:19 +0900

    [BE] OCP, DIP 원칙 지킬 수 있게 코드 수정

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,7 +3,7 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class fixDiscountPolicy implements DiscountPolicy{
+public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000; // 1,000원 할인
 
