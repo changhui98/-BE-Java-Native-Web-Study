624f65e79e15eb96462674273d7140b5970e9cab
changhui98 <net_0102@naver.com>
2025-05-16 23:06:21 +0900

    [BE] 컴파일 오류 나는 부분 수정 DIP 설계 원칙 준수

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -22,6 +22,5 @@ public class OrderServiceImpl implements OrderService {
         int discountPrice = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
-
     }
 }
