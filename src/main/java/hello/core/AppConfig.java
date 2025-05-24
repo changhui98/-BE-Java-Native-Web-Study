ea472c327cf01a8a8ec38b66d7bb13c185546f18
changhui98 <net_0102@naver.com>
2025-05-24 20:09:03 +0900

    [BE] Component , Bean 메타 정보 학습

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -19,6 +19,8 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
@@ -27,11 +29,13 @@ public class AppConfig {
      */
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
